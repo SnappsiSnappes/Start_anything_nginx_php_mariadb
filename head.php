cadcaa8e863717fbc0a148d7df996b8842deab3f
SnappsiSnappes <111605401+SnappsiSnappes@users.noreply.github.com>
2026-04-14 08:43:57 +0300

    Update head.php

--- head.php
+++ head.php
@@ -5,6 +5,7 @@ use {{Name}}\{{App}}\Tools\Tools;
 use {{Name}}\{{App}}\DB\DB;
 use {{Name}}\{{App}}\Config\Config;
 
+Tools::PerformAccessCheck();
 $base_url = Config::getBaseUrl();
 
 ?>
@@ -17,41 +18,46 @@ $base_url = Config::getBaseUrl();
     <title>Новый проект</title>
 
 
-
     <!-- #!! bootstrap -->
     <link rel="stylesheet" href="<?= $base_url ?>css/bootstrap.min.css">
-    <script src="<?= $base_url ?>js/bootstrap.bundle.min.js"></script>
+    <script src="<?= $base_url ?>js/Libres/bootstrap.bundle.min.js"></script>
 
+    <!-- #!! bx24 -->
+    <!-- <script src="<?= $base_url ?>js/Libres/bx24.js"></script> -->
 
     <!-- #!! papa parse js -->
-    <!-- <script src="<?= $base_url ?>js/papaparse.min.js"></script> -->
+    <!-- <script src="<?= $base_url ?>js/Libres/papaparse.min.js"></script> -->
 
     <!-- #!! axios -->
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="<?= $base_url ?>js/Libres/axios.min.js"></script>
 
 
     <!-- #!! htmx -->
-    <script src="<?= $base_url ?>js/htmx.min.js"></script>
+    <script src="<?= $base_url ?>js/Libres/htmx.min.js"></script>
 
     <!-- #!! main -->
     <link rel="stylesheet" href="<?= $base_url ?>css/main.css">
-    <script src="<?= $base_url ?>js/main.js" type="module"></script>
+    <script src="<?= $base_url ?>js/Libres/main.js" type="module"></script>
 
     <!-- #!! jq -->
-    <script src="<?= $base_url ?>js/jq.js"></script>
+    <script src="<?= $base_url ?>js/Libres/jq.js"></script>
 
     <!-- #!! jqui -->
     <link rel="stylesheet" href="<?= $base_url ?>css/jquery-ui.min.css">
-    <script src="<?= $base_url ?>js/jquery-ui.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="<?= $base_url ?>js/Libres/jquery-ui.min.js"></script>
 
     <!-- #!! butterup библиотека уведомлений -->
     <link rel="stylesheet" href="<?= $base_url ?>css/butterup.min.css">
-    <script src="<?= $base_url ?>js/butterup.min.js"></script>
+    <script src="<?= $base_url ?>js/Libres/butterup.min.js"></script>
 
     <!-- #!!  ico -->
     <link rel="icon" type="image/x-icon" href="<?= $base_url ?>ok.ico">
 
+
+
+    <!--  https://www.jqueryscript.net/form/drag-drop-image-uploader.html -->
+    <!-- <script src="<?= $base_url ?>js/Libres/image-uploader.min.js"></script> -->
+    <!-- <link rel="stylesheet" href="<?= $base_url ?>css/image-uploader.min.css"> -->
 </head>
 
 <body class=''>
